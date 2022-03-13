28ee0eb694e72d7758c8ecbc2750e4a0deeab49b
TeddyTrqt <teddy.turquet@protonmail.com>
2022-03-13 21:24:33 +0100

    update src

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="page">
+    <div>
+      <header>
+        <img src="./assets/images/logo.png" alt="MDN logo" />
+        <ul class="topnav">
+          <li><a class="active" href="#home">Accueil</a></li>
+          <li><a href="#news">Qui sommes nous?</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </header>
+      <section>
+        <div>
+          <article class="big">
+            <h1>J'agis à mon échelle</h1>
+            <p>
+              Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto
+              nihil explicabo perferendis quos provident delectus ducimus
+              necessitatibus reiciendis optio tempora unde earum doloremque
+              commodi laudantium ad nulla vel odio?
+            </p>
+            <br />
+            <a class="btn" href="#Section1">Lorem Ipsum ></a>
+          </article>
+        </div>
+        <article class="article">
+          <h1 id="Section1">Lorem</h1>
+          <div class="flex">
+            <p style="clear: right">
+              Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto
+              nihil explicabo perferendis quos provident delectus ducimus
+              necessitatibus reiciendis optio tempora unde earum doloremque
+              commodi laudantium ad nulla vel odio?
+            </p>
+            <img src="https://via.placeholder.com/300.png" />
+          </div>
+        </article>
+        <article class="article">
+          <h1>Lorem ipsum</h1>
+          <div class="flex">
+            <img src="https://via.placeholder.com/300.png" />
+            <p style="clear: right">
+              Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto
+              nihil explicabo perferendis quos provident delectus ducimus
+              necessitatibus reiciendis optio tempora unde earum doloremque
+              commodi laudantium ad nulla vel odio?
+            </p>
+          </div>
+        </article>
+      </section>
+    </div>
+    <footer>
+      <div id="copyright">© 2022 J'agis à mon échelle</div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "scss/style";
 </style>

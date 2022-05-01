47fb44980eab918e9e813d93965987cf0feb47fa
TeddyTrqt <teddy.turquet@protonmail.com>
2022-05-01 21:50:03 +0200

    Add login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingPageView from '@/views/LandingPageView.vue'
 import ContactView from '@/views/ContactView.vue'
 import NewsView from '@/views/NewsView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 
 const routes = [
   { path: '/', component: LandingPageView },
   { path: '/contact', component: ContactView },
   { path: '/news', component: NewsView },
+  { path: '/login', component: LoginView },
 ]
 
 const router = createRouter({
---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form">
+    <h2>Connectez-vous!</h2>
+    <form>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          v-model="username"
+          name="username"
+          class="form-control"
+        />
+      </div>
+      <div class="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          v-model="password"
+          name="password"
+          class="form-control"
+        />
+      </div>
+      <div class="form-group">
+        <button class="btn">Connexion</button>
+      </div>
+      <div class="link-group">
+        <router-link to="/register">Inscrivez-vous</router-link><br />
+        <router-link to="/forgot-password">Mot de passe oublié</router-link>
+      </div>
+    </form>
+  </div>
+</template>
